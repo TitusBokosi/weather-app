a38517cdc9439fa825eccccd607406b53a704a2c
TitusBokosi <titusbokosi@gmail.com>
2025-02-21 10:04:42 +0200

    deployment commit

---
+++ c93c984a1dcaab507cda.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import { loadHomePage } from "./home";
+// import { searchCity } from "./searchCity";
+// // import { loadHomeDefaultCity } from "./homeDeafaultCity";
+// // import { createCityObject } from "./cityObjects";
+// import { cityList } from "./storeCities";
+
+const app = document.querySelector(".app");
+let fix = 1;
+loadHomePage(app);
+
+// async function loadCity(city) {
+//   await searchCity(city);
+//   console.log(cityList[0].country);
+// }
+
+// loadCity("Nairobi");
+// createCityObject("Nairobi", "Kenya", "GMT+3", "12:00", "25°C", "50%");
+// console.log(cityList[0].temperature);
+
+// // loadHomeDefaultCity("Nairobi");
